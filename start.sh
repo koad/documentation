a59b96629f58e625916295a5e57b4bf879be1154
koad on wonderland <wonderland@koad.sh>
2026-03-09 20:47:16 -0400

    Add Python venv setup and mkdocs installation to start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+[ -d .venv ] || python3 -m venv .venv
+
+source .venv/bin/activate
+
+pip show mkdocs-material &>/dev/null || pip install mkdocs-material
+
 mkdocs serve --dev-addr 10.10.10.10:11600
